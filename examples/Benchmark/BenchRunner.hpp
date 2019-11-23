af3af1e2fc38a5793c392de6a341f27889d4a917
Mikhail Pak <code@mp4096.com>
2019-11-23 13:01:38 +0100

    Use brace init

--- examples/Benchmark/BenchRunner.hpp
+++ examples/Benchmark/BenchRunner.hpp
@@ -14,8 +14,8 @@ namespace Benchmark {
 template <typename MatrixType> MatrixType generateTestMatrix(const BenchConfig &conf) {
   std::srand(conf.prngSeed);
 
-  const auto a = MatrixType::Random(conf.numRows, conf.rank);
-  const auto b = MatrixType::Random(conf.rank, conf.numCols);
+  const auto a{MatrixType::Random(conf.numRows, conf.rank)};
+  const auto b{MatrixType::Random(conf.rank, conf.numCols)};
 
   return a * b;
 }
@@ -26,8 +26,8 @@ template <typename MatrixType> class BenchRunnerBase {
 
 public:
   explicit BenchRunnerBase(const BenchConfig &benchConf)
-      : m_testMatrix(generateTestMatrix<MatrixType>(benchConf)), m_benchConfig(benchConf),
-        m_stats() {}
+      : m_testMatrix{generateTestMatrix<MatrixType>(benchConf)},
+        m_benchConfig{benchConf}, m_stats{} {}
 
   virtual void run() = 0;
 
@@ -48,16 +48,16 @@ template <typename MatrixType> class BenchRunnerSvd : public BenchRunnerBase<Mat
 public:
   explicit BenchRunnerSvd(const BenchConfig &benchConf) : BenchRunnerBase<MatrixType>(benchConf) {}
 
-    const auto tic = std::chrono::steady_clock::now();
   void run() final {
+    const auto tic{std::chrono::steady_clock::now()};
     Eigen::JacobiSVD<MatrixType> svd(this->m_testMatrix,
                                      Eigen::ComputeThinU | Eigen::ComputeThinV);
-    const auto toc = std::chrono::steady_clock::now();
+    const auto toc{std::chrono::steady_clock::now()};
 
     this->m_stats.runtime = std::chrono::duration_cast<std::chrono::milliseconds>(toc - tic);
 
-    const MatrixType reconstructed =
-        svd.matrixU() * svd.singularValues().asDiagonal() * svd.matrixV().adjoint();
+    const MatrixType reconstructed{svd.matrixU() * svd.singularValues().asDiagonal() *
+                                   svd.matrixV().adjoint()};
 
     this->m_stats.relativeApproximationError =
         Rsvd::relativeFrobeniusNormError(this->m_testMatrix, reconstructed);
@@ -77,22 +77,22 @@ class BenchRunnerRandomizedSvd : public BenchRunnerBase<MatrixType> {
 
 public:
   BenchRunnerRandomizedSvd(const BenchConfig &benchConf, const RandomizedSvdConfig &rsvdConf)
-      : BenchRunnerBase<MatrixType>(benchConf), m_rsvdConfig(rsvdConf) {}
+      : BenchRunnerBase<MatrixType>(benchConf), m_rsvdConfig{rsvdConf} {}
 
-    std::mt19937_64 randomEngine;
   void run() final {
+    std::mt19937_64 randomEngine{};
     randomEngine.seed(m_rsvdConfig.prngSeed);
 
-    const auto tic = std::chrono::steady_clock::now();
+    const auto tic{std::chrono::steady_clock::now()};
     Rsvd::RandomizedSvd<MatrixType, std::mt19937_64, Conditioner> rsvd(randomEngine);
     rsvd.compute(this->m_testMatrix, m_rsvdConfig.rank, m_rsvdConfig.oversampling,
                  m_rsvdConfig.numIter);
-    const auto toc = std::chrono::steady_clock::now();
+    const auto toc{std::chrono::steady_clock::now()};
 
     this->m_stats.runtime = std::chrono::duration_cast<std::chrono::milliseconds>(toc - tic);
 
-    const MatrixType reconstructed =
-        rsvd.matrixU() * rsvd.singularValues().asDiagonal() * rsvd.matrixV().adjoint();
+    const MatrixType reconstructed{rsvd.matrixU() * rsvd.singularValues().asDiagonal() *
+                                   rsvd.matrixV().adjoint()};
 
     this->m_stats.relativeApproximationError =
         Rsvd::relativeFrobeniusNormError(this->m_testMatrix, reconstructed);
